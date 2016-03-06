c635b616a9fcb8cc9272721a289199c07468a8be
Tim Zenner <epidemicz@gmail.com>
2016-03-06 00:24:18 -0600

    Moved the install script away from being a function.
    Weird stuff was happening, such as getting the message for installing, but still doing the uninstall.

--- contextmenu/script/install-new-dot-file.ps1
+++ contextmenu/script/install-new-dot-file.ps1
@@ -5,82 +5,11 @@
 
 Param ([Boolean]$remove = $false)
 
-function Install-New-Dot-File([Boolean]$remove = $false)
-{
-    $hkcr = Test-Path "HKCR:\"
-
-    if ($hkcr -ne $true)
-    {
-        $registry = New-PSDrive -PSProvider registry -Name HKCR -Root HKEY_CLASSES_ROOT
-    }
-
-    $menupath = "HKCR:\Directory\Background\shell"
-    $menuname = "New .dotfile here"
-    $commandpath = "$menupath\$menuname"
-    $commandname = "command"
-    $commandvalue = "cmd.exe /C newdotfile.bat"
-
-    if ($remove -eq $true)
-    {
-        $exists = Test-Path "$menupath\$menuname"
-
-        if ($exists -eq $true)
-        {
-            # do uninstall
-            # but probably shouldn't force this one :O
-            Remove-Item -Path "$menupath\$menuname" -Recurse *>$null
-        }
-
-        $exists = Test-Path "$menupath\$menuname"
-
-        if ($exists -eq $true)
-        {
-            # failed to write entries
-            Write-Host "Error" -NoNewline -ForegroundColor Red
-            Write-Host "::" -NoNewline
-            Write-Host "Failed to remove: $menupath\$menuname"
-
-            return $false
-        }
-
-        # registry entries were successfully removed
-        Write-Host "Success" -NoNewline -ForegroundColor Green
-        Write-Host "::" -NoNewline
-        Write-Host "Registry entries removed successfully."
-    }
-    else
-    {
-        # do install
-        New-Item -Path $menupath -Name $menuname -Force *>$null
-        New-Item -Path $commandpath -Name $commandname -Force *>$null
-        Set-ItemProperty -Path "$commandpath\$commandname" -Name "(Default)" -Value $commandvalue *>$null
-
-        $created = Test-Path "$commandpath\$commandname"
-
-        if ($created -eq $false)
-        {
-            # failed to write entries
-            Write-Host "Error" -NoNewline -ForegroundColor Red
-            Write-Host "::" -NoNewline
-            Write-Host "Failed to create: $commandpath\$commandname"
-
-            return $false
-        }
-
-        # registry entries were successfully created
-        Write-Host "Success" -NoNewline -ForegroundColor Green
-        Write-Host "::" -NoNewline
-        Write-Host "Registry entries created successfully."
-    }
-
-    return $true
-}
-
-if ($remove -eq $true)
+if ($remove -eq $True)
 {
     Write-Host "Info" -NoNewline -ForegroundColor Cyan
     Write-Host "::" -NoNewline
-    Write-Host "Attempting to remove newdotfile from menu..."    
+    Write-Host "Attempting to remove newdotfile from menu..."
 }
 else
 {
@@ -89,9 +18,75 @@ else
     Write-Host "Attempting to install newdotfile to menu..."
 }
 
-$success = Install-New-Dot-File $remove
+$success = $True
 
-if ($success -eq $true)
+$hkcr = Test-Path "HKCR:\"
+
+if ($hkcr -ne $true)
+{
+    $registry = New-PSDrive -PSProvider registry -Name HKCR -Root HKEY_CLASSES_ROOT
+}
+
+$menupath = "HKCR:\Directory\Background\shell"
+$menuname = "New .dotfile here"
+$commandpath = "$menupath\$menuname"
+$commandname = "command"
+$commandvalue = "cmd.exe /C newdotfile.bat"
+
+if ($remove -eq $True)
+{
+    $exists = Test-Path "$menupath\$menuname"
+
+    if ($exists -eq $True)
+    {
+        # do uninstall
+        # but probably shouldn't force this one :O
+        Remove-Item -Path "$menupath\$menuname" -Recurse *>$null
+    }
+
+    $exists = Test-Path "$menupath\$menuname"
+
+    if ($exists -eq $True)
+    {
+        # failed to write entries
+        Write-Host "Error" -NoNewline -ForegroundColor Red
+        Write-Host "::" -NoNewline
+        Write-Host "Failed to remove: $menupath\$menuname"
+
+        return
+    }
+
+    # registry entries were successfully removed
+    Write-Host "Success" -NoNewline -ForegroundColor Green
+    Write-Host "::" -NoNewline
+    Write-Host "Registry entries removed successfully."
+}
+else
+{
+    # do install
+    New-Item -Path $menupath -Name $menuname -Force *>$null
+    New-Item -Path $commandpath -Name $commandname -Force *>$null
+    Set-ItemProperty -Path "$commandpath\$commandname" -Name "(Default)" -Value $commandvalue *>$null
+
+    $created = Test-Path "$commandpath\$commandname"
+
+    if ($created -eq $false)
+    {
+        # failed to write entries
+        Write-Host "Error" -NoNewline -ForegroundColor Red
+        Write-Host "::" -NoNewline
+        Write-Host "Failed to create: $commandpath\$commandname"
+
+        $success = $False
+    }
+
+    # registry entries were successfully created
+    Write-Host "Success" -NoNewline -ForegroundColor Green
+    Write-Host "::" -NoNewline
+    Write-Host "Registry entries created successfully."
+}
+
+if ($success -eq $True)
 {
     Write-Host "Success" -NoNewline -ForegroundColor Green
     Write-Host "::" -NoNewline
